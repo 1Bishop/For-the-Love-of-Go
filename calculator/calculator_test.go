348d019006d53910a13294ac4326437183c806d9
Chris Bishop <chris@bishoptech.io>
2023-05-01 08:06:42 -0400

    Working on Chapter 2

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -7,6 +7,26 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 2, want: 3},
+		{a: 0, b: 0, want: 0},
+		{a: -1, b: -1, want: -2},
+		{a: -1, b: 1, want: 0},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+
 	var want float64 = 4
 	got := calculator.Add(2, 2)
 	if want != got {
