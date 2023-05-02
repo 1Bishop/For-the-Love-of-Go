b0532e8a017f1c30030e82716799655a8e7a2cbc
Chris Bishop <chris@bishoptech.io>
2023-05-02 08:21:30 -0400

    Finished new tests for Add, Subtract & Multiply

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -5,12 +5,13 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	a, b float64
+	want float64
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	type testCase struct {
-		a, b float64
-		want float64
-	}
 
 	testCases := []testCase{
 		{a: 2, b: 2, want: 4},
@@ -36,6 +37,22 @@ func TestAdd(t *testing.T) {
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
+
+	testCases := []testCase{
+		{a: 2, b: 2, want: 0},
+		{a: 1, b: 2, want: -1},
+		{a: 0, b: 0, want: 0},
+		{a: -1, b: -1, want: 0},
+		{a: -1, b: 1, want: -2},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+
 	var want float64 = 2
 	got := calculator.Subtract(4, 2)
 	if want != got {
@@ -45,6 +62,22 @@ func TestSubtract(t *testing.T) {
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
+
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 2, want: 2},
+		{a: 0, b: 0, want: 0},
+		{a: -1, b: -1, want: 1},
+		{a: -1, b: 1, want: -1},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+
 	var want float64 = 4
 	got := calculator.Multiply(2, 2)
 	if want != got {
