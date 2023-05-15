2170e36232d48567e54e3792667815af0b02c5a9
Chris Bishop <chris@bishoptech.io>
2023-05-15 19:39:06 -0400

    working on bookstore and learning GoLand

---
+++ bookstore/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var title string
+	var copies int
+	title = "For the Love of Go"
+	copies = 99
+	var author string = "Alan A.A. Donovan"
+	var edition float64 = 1.0
+	var inStock bool = true
+	var royaltyPercent float64 = 12.5
+	var onSale bool = false
+	var salePrice float64 = 0.9
+	boop := "boop"
+	fmt.Println(boop)
+
+	fmt.Println("Book:", title, "Copies:", copies, "Author:", author)
+
+}
